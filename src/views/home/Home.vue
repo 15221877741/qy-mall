4cd59d26c1e3200fcb18f8d2ef515ffb8c18bdf6
15221877741 <qingyangwang113@163.com>
2020-07-14 21:12:56 +0800

    首页商品数据展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,73 +8,34 @@
   <recommend-view :recommends="recommends"></recommend-view>
   <!--  -->
   <feature></feature>
+  <!-- 流行 新款 精选 -->
   <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
-  <ul>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-    <li>123</li>
-  </ul>
+  <!-- 商品展示 -->
+  <goods-list :goods="goods['pop'].list"></goods-list>
 </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
 
 import HomeSwiper from './chilComps/HomeSwiper'
 import RecommendView from './chilComps/RecommendView'
 import Feature from './chilComps/FeatureView'
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata,getHomeGoods } from "network/home";
 export default {
   name: 'Home',
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      }
     }
   },
   components: {
@@ -82,15 +43,34 @@ export default {
     HomeSwiper,
     RecommendView,
     Feature,
-    TabControl
+    TabControl,
+    GoodsList
   },
   created() {
     // 请求多个数据
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list
-      this.recommends = res.data.recommend.list
-      console.log(res)
-    })
+    this.getHomeMultidata()
+
+    //请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+        console.log(res)
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page +1
+      getHomeGoods(type,page).then(res => {
+        // console.log(res)
+        this.goods[type].page += 1
+        this.goods[type].list.push(...res.data.list)
+      })
+    }
   }
 }
 </script>
